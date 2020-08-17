e9ebe5b5e80c31d8f9f622710d9f956b94c730db
mercesolis <mersolis@live.com>
2020-08-17 18:54:25 -0500

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ModelComponent } from './model/model.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent,
+    ModelComponent,
+    CarouselComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
