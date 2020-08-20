ace974afd978f8abdd3eb1aa0840d71c08c4fb80
mercesolis <mersolis@live.com>
2020-08-19 22:52:28 -0500

    replaced hard code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './model/iproduct';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  product: IProduct[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.product = this.productService.getProducts();
+  }
 }
