e1247c859cc03bda5899a8dde03a6f819e1fa341
mercesolis <mersolis@live.com>
2020-08-19 22:24:13 -0500

    add product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { IProduct } from './model/iproduct';
+import { products } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  shoppingCart: IProduct[] = [];
+  products: IProduct[] = [];
+
+  constructor() { 
+    this.products = products;
+  }
+
+  getProducts(): IProduct[] {
+    return this.products;
+  }
+
+  getShoppingCart(): IProduct[] {
+    return this.shoppingCart;
+  }
+
+  addToCart(product: IProduct): void {
+    this.shoppingCart.push(product);
+  }
 }
